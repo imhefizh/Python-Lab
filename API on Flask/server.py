42734dfcc245f090c66d1be21694c54917d9a7ed
imhefizh <hafidzsml@gmail.com>
2025-10-17 11:05:18 -0400

    init

---
+++ API on Flask/server.py
@@ -0,0 +1,148 @@
+# Import the Flask class from the flask module
+from flask import Flask, make_response, request
+
+# Create an instance of the Flask class, passing in the name of the current module
+app = Flask(__name__)
+
+data = [
+    {
+        "id": "3b58aade-8415-49dd-88db-8d7bce14932a",
+        "first_name": "Tanya",
+        "last_name": "Slad",
+        "graduation_year": 1996,
+        "address": "043 Heath Hill",
+        "city": "Dayton",
+        "zip": "45426",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/139x100.png/cc0000/ffffff",
+    },
+    {
+        "id": "d64efd92-ca8e-40da-b234-47e6403eb167",
+        "first_name": "Ferdy",
+        "last_name": "Garrow",
+        "graduation_year": 1970,
+        "address": "10 Wayridge Terrace",
+        "city": "North Little Rock",
+        "zip": "72199",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/148x100.png/dddddd/000000",
+    },
+    {
+        "id": "66c09925-589a-43b6-9a5d-d1601cf53287",
+        "first_name": "Lilla",
+        "last_name": "Aupol",
+        "graduation_year": 1985,
+        "address": "637 Carey Pass",
+        "city": "Gainesville",
+        "zip": "32627",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/174x100.png/ff4444/ffffff",
+    },
+    {
+        "id": "0dd63e57-0b5f-44bc-94ae-5c1b4947cb49",
+        "first_name": "Abdel",
+        "last_name": "Duke",
+        "graduation_year": 1995,
+        "address": "2 Lake View Point",
+        "city": "Shreveport",
+        "zip": "71105",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/145x100.png/dddddd/000000",
+    },
+    {
+        "id": "a3d8adba-4c20-495f-b4c4-f7de8b9cfb15",
+        "first_name": "Corby",
+        "last_name": "Tettley",
+        "graduation_year": 1984,
+        "address": "90329 Amoth Drive",
+        "city": "Boulder",
+        "zip": "80305",
+        "country": "United States",
+        "avatar": "http://dummyimage.com/198x100.png/cc0000/ffffff",
+    }
+]
+
+# Define a route for the root URL ("/")
+@app.route("/")
+def index():
+    # Function that handles requests to the root URL
+    # Return a plain text response
+    resp = make_response({"msg":"Hellow World"})
+    resp.status_code = 300
+    return resp
+
+@app.route("/data")
+def get_data():
+    try:
+        if data and len(data) > 0:
+            return {"message": f"Data of length {len(data)} found"}
+        else:
+            return {"message": "Data is empty"}, 500
+    except NameError:
+        return {"message": "Data not found"}, 404
+
+@app.route("/name_search")
+def search_data_by_name():
+    query = request.args.get('q')
+
+    if query is None:
+        return {"message": "Query parameter 'q' is missing"}, 400
+
+    if query.strip() == "" or query.isdigit():
+        return {"message":"Invalid input parameter"}, 422
+
+    for person in data:
+        if query.lower() in person["first_name"].lower():
+            return person, 200
+
+    return {"message":"Person not found"}, 404
+
+@app.route("/count")
+def count():
+    return {"result":f"Number of data is {len(data)}"}
+
+@app.route("/person/<uuid>", methods=["GET", "DELETE"])
+def middleware(uuid):
+    if request.method == "DELETE":
+        if uuid is None:
+            return {"message":"Please provide the UUID"}, 400
+        elif uuid.isdigit():
+            return {"message":"Please provide proper UUID"}, 400
+        else:
+            for person in data:
+                if person['id'] == uuid:
+                    data.remove(person)
+                    return {"message":"Deleted"}, 200
+            return {"message":"I think you put the wrong ID, sir!"}, 400
+        return {"message":"Something wrong here, idk"}, 500
+    elif request.method == "GET":
+        if uuid is None:
+            return {"message":"Please provide the UUID"}, 400
+        elif uuid.isdigit():
+            return {"message":"Please provide proper UUID"}, 400
+        else:
+            for person in data:
+                if person['id'] == uuid:
+                    return person, 200
+            return {"message":"I think you put the wrong ID, sir!"}, 404
+        return {"message":"Something wrong here, idk"}, 500
+
+@app.route("/person", methods=["POST"])
+def new_data():
+    data = request.get_json()
+
+    if not data:
+        return {"message":"Where is the data?"}, 400
+    return {"message":"Created"}, 200 
+
+@app.errorhandler(404)
+def error(error):
+    return {"message":"U DID SOMETHING DIRTY"}, 404
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    return {"message": str(e)}, 500
+
+@app.route("/test500")
+def test500():
+    raise Exception("Forced exception for testing")
