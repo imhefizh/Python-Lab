7a2e4f927b2dff5025c0d2f33ad685cfd27e1172
imhefizh <hafidzsml@gmail.com>
2025-10-23 22:23:07 -0400

    Chatbot iFace

--- Chatbot with WebInterface/WebInterface/app.py
+++ Chatbot with WebInterface/WebInterface/app.py
@@ -1,43 +1,48 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS	
-from transformers import AutoModelForSeq2SeqLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = "facebook/blenderbot-400M-distill"
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-conversation_history = []
+conversation_history = [{"role": "system", "content": "You are a helpful AI assistant."}]
 
 app = Flask(__name__)
 CORS(app)				
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/chatbot', methods=['POST'])
 def handle_prompt():
     # Read prompt from HTTP request body
     data = request.get_data(as_text=True)
     data = json.loads(data)
-    input_text = data['prompt']
-
-    # Create conversation history string
-    history = "\n".join(conversation_history)
 
     # Tokenize the input text and history
-    inputs = tokenizer.encode_plus(history, input_text, return_tensors="pt")
+    
+    messages = {"role": "user", "content": data['prompt']}
+    conversation_history.append(messages)
+
+    inputs = tokenizer.apply_chat_template(
+	    conversation_history,
+	    add_generation_prompt=True,
+	    tokenize=True,
+	    return_dict=True,
+	    return_tensors="pt",
+    ).to(model.device)
 
     # Generate the response from the model
-    outputs = model.generate(**inputs, max_length= 60)  # max_length will acuse model to crash at some point as history grows
+    outputs = model.generate(**inputs, max_new_tokens=40)  # max_length will acuse model to crash at some point as history grows
 
     # Decode the response
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True).strip()
+    response = tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:])
 
     # Add interaction to conversation history
-    conversation_history.append(input_text)
-    conversation_history.append(response)
+    conversation_history.append({"role": "assistant", "content": response})
+    print(conversation_history)
 
     return response
 
